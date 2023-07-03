8355d0b19748e67a9a38cad6eb57505cd33f911a
Zachary Laney <zacharylaney@gmail.com>
2023-07-03 10:56:40 -0500

    Frontend general template initial template
    This template looks basic enough to satisfy the project requirements. Now I just need to create the SQL ingestion piece and wire up the frontend UI.

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,96 +1,80 @@
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent } from "vue";
+import { GoogleMap, Marker } from "vue3-google-map";
 import GridFromServer from '/@src/components/GridFromServer.vue'
 
 import { getTestItems } from '/@src/api/getTestItems';
 
-import { ref } from 'vue'
+import { ref } from 'vue';
 
-defineProps<{ msg: string }>()
+export default defineComponent({
+  components: { GoogleMap, Marker, GridFromServer },
+  setup() {
+    const center = { lat: 40.689247, lng: -74.044502 };
 
-const count = ref(0)
+    const selectedCadFile = ref("No CAD File Selected");
 
-const gridServerDataSource = async () => {
-  return {
-    getRows: async function (params: any) {
-      return await getTestItems()
-        .then(async (data) => {
-          params.success({ rowData: data });
-        })
-        .catch(() => {
-          params.fail();
-        });
-    },
-  };
-};
+    const gridServerDataSource = async () => {
+      return {
+        getRows: async function (params: any) {
+          return await getTestItems()
+            .then(async (data) => {
+              params.success({ rowData: data });
+            })
+            .catch(() => {
+              params.fail();
+            });
+        },
+      };
+    };
 
-const gridServerDataOptions = {
-  columnDefs: [
-    { field: 'id', mindWidth: 50, flex: 1 },
-    { field: 'data', flex: 2, minWidth: 500 },
-  ],
-  defaultColDef: {
-    sortable: true,
-    resizable: true,
+    const gridServerDataOptions = {
+      columnDefs: [
+        { field: 'id', mindWidth: 50, flex: 1 },
+        { field: 'data', flex: 2, minWidth: 500 },
+      ],
+      defaultColDef: {
+        sortable: true,
+        resizable: true,
+      },
+      rowModelType: 'serverSide',
+      serverSideStoreType: 'full',
+      animateRows: true,
+    };
+
+    return { center, gridServerDataSource, gridServerDataOptions, selectedCadFile };
   },
-  rowModelType: 'serverSide',
-  serverSideStoreType: 'full',
-  animateRows: true,
-};
-
-async function test() {
-  const result = await getTestItems();
-  alert(JSON.stringify(result));
-}
-
+});
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <p>
-      Count: {{ count }}
-    </p>
-  </div>
-
-  <div>
-    <BButton @click="test()" variant="outline-primary">Test</BButton>
-    <BButton @click="count++" variant="danger">Increment</BButton>
-    <BButton @click="count++" variant="success">Increment</BButton>
-    <BButton @click="count++" variant="outline-primary">Increment</BButton>
-  </div>
-
-  <BCardGroup deck>
-  <BCard
-    header="featured"
-    header-tag="header"
-    footer="Card Footer"
-    footer-tag="footer"
-    title="Title"
-  >
-    <BCardText>Header and footers using props.</BCardText>
-    <BButton href="#" variant="primary">Go somewhere</BButton>
+  <h1 style="position:center">Quick Computer Aided Response (CAD) Viewing Tool</h1>
+  <BAccordion>
+  <BAccordionItem title="Upload CAD File">
+    Upload Here
+  </BAccordionItem>
+  <BAccordionItem title="Select CAD File">
+    <BCard title="CAD File" style="position:center; mwidth: 100rem;">
+    <GridFromServer :server-data-source="gridServerDataSource" :options="gridServerDataOptions" />
   </BCard>
-
-  <BCard title="Title" header-tag="header" footer-tag="footer">
-    <template #header>
-      <h6 class="mb-0">Header Slot</h6>
-    </template>
-    <BCardText>Header and footers using slots.</BCardText>
-    <BButton href="#" variant="primary">Go somewhere</BButton>
-    <template #footer>
-      <em>Footer Slot</em>
-    </template>
+  </BAccordionItem>
+  <BAccordionItem title="View Selected CAD File Location Details">
+    <h2>{{ selectedCadFile }}</h2>
+    <BCardGroup deck>
+    <BCard title="Google Map Location" style="position:center; width: 50rem;">
+    <GoogleMap api-key="AIzaSyAx649U3JEg5Z_Xy2WXfmLF9wYH0ofm4rk" style="width: 100%; height: 500px" :center="center"
+      :zoom="15">
+      <Marker :options="{ position: center }" />
+    </GoogleMap>
   </BCard>
-</BCardGroup>
+  <BCard title="Historical weather at time and place" style="position:center; width: 50rem;">
+    Weather here
+  </BCard>
+  </BCardGroup>
+  </BAccordionItem>
+</BAccordion>
 
-
-<GridFromServer
-      :server-data-source="gridServerDataSource"
-      :options="gridServerDataOptions"
-    />
 
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
